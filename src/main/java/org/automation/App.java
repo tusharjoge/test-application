6b0243ff7fe2e81d6e8cdb31f1fe3725eb336486
tusharjoge <tdjoge@hotmail.com>
2020-03-02 19:35:47 +0000

    Testing Jenkins Setup

---
+++ src/main/java/org/automation/App.java
@@ -0,0 +1,13 @@
+package org.automation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! - Let us test our application on Jenkins" );
+    }
+}
